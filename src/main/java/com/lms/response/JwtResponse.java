d6677937388739eef377279b57f0592caa06a3a8
Milan Bačkonja <milan.backonja@hotmail.com>
2019-09-09 12:24:29 +0200

    Authentification and authorization

--- src/main/java/com/lms/response/JwtResponse.java
+++ src/main/java/com/lms/response/JwtResponse.java
@@ -1,26 +1,26 @@
 package com.lms.response;
 
 public class JwtResponse {
-    private String token;
-    private String type = "Bearer";
- 
-    public JwtResponse(String accessToken) {
-        this.token = accessToken;
-    }
- 
-    public String getAccessToken() {
-        return token;
-    }
- 
-    public void setAccessToken(String accessToken) {
-        this.token = accessToken;
-    }
- 
-    public String getTokenType() {
-        return type;
-    }
- 
-    public void setTokenType(String tokenType) {
-        this.type = tokenType;
-    }
+	private String token;
+	private String type = "Bearer";
+
+	public JwtResponse(String token) {
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getTokenType() {
+		return type;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.type = tokenType;
+	}
 }
