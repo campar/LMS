24278fef551ef98d73cf15d8b288f19623ca1aa9
Milan Bačkonja <milan.backonja@hotmail.com>
2019-09-23 10:33:08 +0200

    Fixed query

--- src/main/java/com/lms/repository/StudentRepository.java
+++ src/main/java/com/lms/repository/StudentRepository.java
@@ -12,6 +12,6 @@ public interface StudentRepository extends CrudRepository<Student, Long> {
 	@Query("FROM Student s WHERE s.name LIKE %?1% AND s.lastName LIKE %?2%")
 	List<Student> findBySearch(String name, String lastName);
 
-	@Query(value = "SELECT DISTINCT st.* FROM year_of_study_subject as yoss JOIN subject as s ON yoss.subject_id = s.id JOIN student_year_of_study as syos ON syos.year_of_study_id = yoss.year_of_study_id JOIN student as st ON st.id = syos.student_id WHERE yoss.subject_id = ?1 AND syos.year_of_study_id = ?2", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT us.*, st.* FROM year_of_study_subject as yoss JOIN subject as s ON yoss.subject_id = s.id JOIN student_year_of_study as syos ON syos.year_of_study_id = yoss.year_of_study_id JOIN student as st ON st.id = syos.student_id JOIN users as us ON us.id = st.id WHERE yoss.subject_id = ?1 AND syos.year_of_study_id = ?2", nativeQuery = true)
 	List<Student> getStudentsByYearBySubject(int subjectId, int yearId);
 }
